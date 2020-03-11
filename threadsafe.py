7b066124c75b5636e6cd8b05c4a11994861cff63
松本　慶太 <emuemu@matsumotokeitanoMacBook-Air.local>
2020-03-11 23:00:00 +0900

    threadsafe study

---
+++ threadsafe.py
@@ -0,0 +1,24 @@
+import threading
+from concurrent.futures import ThreadPoolExecutor, wait
+
+
+class ThreadSafeCounter:
+    lock = threading.Lock()
+    def __init__(self):
+        self.count = 0
+    def increment(self):
+        with self.lock:
+            self.count = self.count + 1
+
+def count_up(counter):
+    for _ in range(10000):
+        counter.increment()
+
+counter = ThreadSafeCounter()
+threads = 2
+
+with ThreadPoolExecutor() as e:
+    futures = [e.submit(count_up, counter) for _ in range(threads)]
+    done, not_done = wait(futures)
+
+print(f'{counter.count=:,}')
